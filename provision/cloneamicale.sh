8a712b2ef1b1f884dd1e1645115dbed99376629c
Yoann Fleury <yoann.fleury@yahoo.com>
2016-01-17 22:11:12 +0100

    fix(provision) : update repository link

--- provision/cloneamicale.sh
+++ provision/cloneamicale.sh
@@ -2,6 +2,6 @@
 
 rm -rf /var/www/html/*
 rm -rf /var/www/html/.git
-git clone https://github.com/yoannfleurydev/amicale.git /var/www/html/
+git clone https://github.com/yoannfleurydev/amicale-src.git /var/www/html/
 touch /var/www/html/.gitkeep
 rm -rf /var/www/amicale
