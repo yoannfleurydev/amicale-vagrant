34997125135a8c5d8da8a59b8cf978306a7ec739
Yoann Fleury <yoann.fleury@yahoo.com>
2016-02-03 21:55:57 +0100

    feat(phpmyadmin) : picture may now display everywhere on pma

--- puphpet/files/exec-once/phpmyadmin.sh
+++ puphpet/files/exec-once/phpmyadmin.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
 
- mv /var/www/vhosts/phpmyadmin.conf /etc/nginx/sites-available
+cp /var/www/vhosts/phpmyadmin.conf /etc/nginx/sites-available/phpmyadmin.conf
 ln -s /etc/nginx/sites-available/phpmyadmin.conf /etc/nginx/sites-enabled/phpmyadmin.conf
 chown root:root /etc/nginx/sites-available/phpmyadmin.conf
+sudo chmod 644 /etc/nginx/sites-available/phpmyadmin.conf
 
-wget https://github.com/phpmyadmin/phpmyadmin/archive/RELEASE_4_5_4_1.zip
+wget https://github.com/phpmyadmin/phpmyadmin/archive/RELEASE_4_5_4_1.zip -nv
 mkdir /var/www/
 unzip RELEASE_4_5_4_1.zip -d /var/www/
 mv /var/www/phpmyadmin-RELEASE_4_5_4_1 /var/www/phpmyadmin
 
-
 service nginx restart
 
-rm -rf /var/www/vhosts
 rm -rf RELEASE_4_5_4_1.zip
