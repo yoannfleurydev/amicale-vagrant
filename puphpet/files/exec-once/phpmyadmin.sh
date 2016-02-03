d9287aba1fa491847b469f05ebe4eea78e2eed2e
Yoann Fleury <yoann.fleury@yahoo.com>
2016-02-03 14:55:20 +0100

    feat(phpmyadmin) : add pma via provisionning

--- puphpet/files/exec-once/phpmyadmin.sh
+++ puphpet/files/exec-once/phpmyadmin.sh
@@ -2,4 +2,11 @@
 
 mv /var/www/phpmyadmin/phpmyadmin.conf /etc/nginx/sites-available
 ln -s /etc/nginx/sites-available/phpmyadmin.conf /etc/nginx/sites-enabled/phpmyadmin.conf
+chown root:root /etc/nginx/sites-available/phpmyadmin.conf
+
+wget https://github.com/phpmyadmin/phpmyadmin/archive/RELEASE_4_5_4_1.zip
+mkdir /var/www/
+unzip RELEASE_4_5_4_1.zip -d /var/www/
+mv /var/www/phpmyadmin-RELEASE_4_5_4_1 /var/www/phpmyadmin
+
 service nginx restart
