958a6a579f8404e386eb48faf3026b24c5d9d24b
Yoann Fleury <yoann.fleury@yahoo.com>
2016-02-03 15:46:36 +0100

    fix(phpmyadmin) : wrong path for pma vhost

--- puphpet/files/exec-once/phpmyadmin.sh
+++ puphpet/files/exec-once/phpmyadmin.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-mv /var/www/phpmyadmin/phpmyadmin.conf /etc/nginx/sites-available
+ mv /var/www/vhosts/phpmyadmin.conf /etc/nginx/sites-available
 ln -s /etc/nginx/sites-available/phpmyadmin.conf /etc/nginx/sites-enabled/phpmyadmin.conf
 chown root:root /etc/nginx/sites-available/phpmyadmin.conf
 
@@ -9,4 +9,8 @@ mkdir /var/www/
 unzip RELEASE_4_5_4_1.zip -d /var/www/
 mv /var/www/phpmyadmin-RELEASE_4_5_4_1 /var/www/phpmyadmin
 
+
 service nginx restart
+
+rm -rf /var/www/vhosts
+rm -rf RELEASE_4_5_4_1.zip
